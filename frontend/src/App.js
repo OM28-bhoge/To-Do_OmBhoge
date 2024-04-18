e4aab0380e71e6b87b5f131545cdd20f158a309e
Om Bhoge <bhogeom28@gmail.com>
2024-04-18 14:05:59 +0530

    Initial Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,12 @@
-import React from 'react';
-import TodoList from './components/TodoList';
-import AddTodoForm from './components/AddTodoForm';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './graphql/client';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import KeycloakProvider from './auth/KeycloakProvider';
-import PrivateRoute from './auth/PrivateRoute';
-import PaymentForm from './components/PaymentForm';
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
 
-const App = () => {
+function App() {
   return (
-    <ApolloProvider client={client}>
-      <KeycloakProvider>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={TodoList} />
-            <PrivateRoute exact path="/add" component={AddTodoForm} />
-            <PrivateRoute exact path="/payment" component={PaymentForm} />
-          </Switch>
-        </Router>
-      </KeycloakProvider>
-    </ApolloProvider>
+    <div className="App">
+      <TodoWrapper  />
+    </div>
   );
-};
+}
 
 export default App;
